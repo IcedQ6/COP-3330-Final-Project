f1492bfb2b928883d8c2dbad1d8606d0c186f14c
yvvtvft <145813536+yvvtvft@users.noreply.github.com>
2025-04-17 20:48:42 -0400

    Update Faculty.java

--- FinalProject/src/allClasses/Faculty.java
+++ FinalProject/src/allClasses/Faculty.java
@@ -1,11 +1,17 @@
+
 package allClasses;
-
-//Engineer's code (replace with full name)
-
+ //Ivan
+ 
 public class Faculty extends Employee {
+    private String rank;
 
-	
-	public void print() {
-		
-	}
+    public Faculty(String fullName, String id, String department, String rank) {
+        super(fullName, id, department);
+        this.rank = rank;
+    }
+
+    @Override
+    public void print() {
+        System.out.printf("%s\nID: %s\n%s, %s\n", fullName, id, rank, department);
+    }
 }
